7d996f6ddb5a79461afa2b5584b0f7035d95b85c
Nico <nico.vnx07@gmail.com>
2021-11-25 00:53:06 -0300

    user

--- index.js
+++ index.js
@@ -48,12 +48,12 @@ app.use(
 
 // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
 // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   if (req.cookies.user_sid && !req.session.user) {
     res.clearCookie("user_sid");
   }
   next();
-});
+});*/
 
 // middleware function to check for logged-in users
 var sessionChecker = (req, res, next) => {
